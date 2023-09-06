f9d46a7d39e3c622dabbae4fd67f2ec632d84608
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2023-09-06 20:57:17 +0530

    added interfaces of respitory and services

---
+++ respository/ProductRepository.go
@@ -0,0 +1,11 @@
+package respository
+
+import "go_microservice_learning_1/models"
+
+type ProductRepository interface {
+	FindById(id int) (models.Product, error)
+	Save(product models.Product) error
+	Update(product models.Product) error
+	Delete(id int) error
+	FindAll() ([]models.Product, error)
+}
---
+++ services/ProductService.go
@@ -0,0 +1,11 @@
+package services
+
+import "go_microservice_learning_1/models"
+
+type ProductService interface {
+	GetProductById(id int) (models.Product, error)
+	AddProduct(product models.Product) error
+	RemoveProduct(id int) error
+	EditProduct(product models.Product) error
+	GetAllProducts() ([]models.Product, error)
+}
